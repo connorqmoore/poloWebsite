856b0c4db152ab679d8ec86dd8d227228c9fc679
Connor <connorqmoore@gmail.com>
2014-10-22 13:15:30 -0400

    Polo website

---
+++ js/js.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+  $( "#a_team" ).click(function( event ) {
+    $('#team').ScrollTo();
+  });
+
+  $( "#a_roster" ).click(function( event ) {
+    $('#roster').ScrollTo();
+  });
+
+  $( "#a_practice" ).click(function( event ) {
+    $('#practice').ScrollTo();
+  });
+});
