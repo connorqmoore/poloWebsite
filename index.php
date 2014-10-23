8e2482cc77c1753adaca9f8b8195dca0aea1009b
Evans Hauser <evanshauser@gmail.com>
2014-10-23 01:05:59 -0400

    added hard linked html for easier debugging

--- index.php
+++ index.php
@@ -1,1 +1,128 @@
-./index.html
+<!DOCTYPE html>
+
+<html class="no-js" lang="en">
+  <head>
+    <!--Google Font Roboto-->
+    <link href='http://fonts.googleapis.com/css?family=Muli:300italic,400' rel='stylesheet' type='text/css'>
+    <!--Title and metadata-->
+    <meta charset="utf-8">
+    <meta content="width=device-width,initial-scale=1.0" name="viewport">
+    <title>CMU Water Polo</title>
+    <!--CSS-->
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--JS-->
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/jquery-scrollto.js"></script>
+    <script src="js/js.js"></script>
+
+    <!--Favicon-->
+  <link rel="icon"
+        type="image/ico"
+        href="img/icons/favicon.ico">
+  </head>
+  <body>
+      <div class="wrapper">
+
+        <div class="info-bar">
+          <div class="container">
+            <a id="a_team">Team</a>
+            <a id="a_roster">Roster</a>
+            <a id="a_practice">Practice</a>
+          </div>
+        </div>
+
+        <section class="module parallax parallax-1">
+          <div id="container">
+            <h1 id="heading">CMU Water Polo</h1>
+          </div>
+        </section>
+
+        <section class="module content">
+          <div class="container">
+            <h2 id="team">Team</h2>
+            <p>CMU Water Polo is a Club sport at Carnegie Mellon University that competes at the NCAA divison 3 club level. Our team is comprised of players from all different background. We pride ourselves on our diversity, but our number one concentration is, and will continue to be competing at a national level. During the 2013 season we competed at Franklin Marshall, and this season we have qualified to participate at the national competition at Bowdin Maine. </p>
+          </div>
+        </section>
+
+        <section class="module parallax parallax-2">
+          <div class="container">
+            <h1>Roster</h1>
+          </div>
+        </section>
+
+        <section class="module content">
+          <div class="container">
+            <h2 id="roster">Roster</h2>
+            <div class="players">
+              <div class="freshman">
+                <h3>Freshman</h3>
+                <ul>
+                  <li>Alan Hodge</li>
+                  <li>Eli Peterson</li>
+                  <li>Nate Horan</li>
+                  <li>Philip Forte</li>
+                  <li>Vivaan Bahl</li>
+                </ul>
+              </div>
+              <div class="sophmore">
+                <h3>Sophmore</h3>
+                <ul>
+                  <li>Brent Tan</li>
+                  <li>Connor Moore</li>
+                  <li>Evans Hauser</li>
+                  <li>Quincy Liu</li>
+                  <li>Steffen Holm</li>
+                </ul>
+              </div>
+              <div class="junior">
+                <h3>Junior</h3>
+                <ul>
+                  <li>Justin Hilliard</li>
+                  <li>Luke Bruce</li>
+                  <li>Luke Sampson</li>
+                  <li>Patrick Koenig</li>
+                  <li>Theodore Warhoe</li>
+                </ul>
+              </div>
+              <div class="senior">
+                <h3>Senior</h3>
+                <ul>
+                  <li>Matthew Reid</li>
+                  <li>Nicky Ong</li>
+                  <li>Yannick Hogarth</li>
+                </ul>
+              </div>
+              <div class="grad">
+                <h3>Graduate Students</h3>
+                <ul>
+                  <li>Ben Solecki</li>
+                  <li>Constantine Nakos</li>
+                  <li>Rob Saul</li>
+                  <li>Willy Elmore (Coach)</li>
+                </ul>
+              </div>
+            </div>
+          </div> <!--Container-->
+        </section>
+
+        <section class="module parallax parallax-3">
+          <div class="container">
+            <h1>Practice</h1>
+          </div>
+        </section>
+
+        <section class="module content">
+          <div class="container">
+            <h2 id="practice">Practice</h2>
+              <p>Practice is <strong>Monday, Wednesday, and Sunday</strong> from <strong>9 to 11</strong>. We meet at the UC pool and have an optional swim workout at 8pm-9pm at </p>
+          </div>
+        </section>
+      </div>
+      </main>
+    </body>
+  <footer>
+    Website maintained by Connor Moore. Current as of Fall 2014.
+  </footer>
+</html>
